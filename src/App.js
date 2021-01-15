b15d56c5bc7a7cbbaf0f398b74e42340276aba68
RoseAsuko <KimberlyJModeste@gmail.com>
2021-01-15 13:46:50 -0500

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
+import DropdownButton from 'react-bootstrap/DropdownButton'
+import Dropdown from 'react-bootstrap/Dropdown'
 import './App.css';
+import { connect } from './redux.js';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <table>
+      <tr><td><button onClick={"ADD ROW"} id = "addRow" type = "button">Add Row</button></td>
+      <td><button onClick={"ADD COLUMN"} id = "addColumns" type = "button">Add Columns</button></td>
+      
+      <select name = "Colors" id ="colors">
+        <option>Red</option>
+        <option>Gold</option>
+        <option>Black</option>
+        <option>Blue</option>
+        <option>Green</option>
+        <option>Orange</option>
+        <option>white</option>
+      </select>
+      </tr> 
+      
+      <tr><td><button onClick={"SUB ROW"}  id = "subRow" type = "button">Subtract Row</button></td>
+      <td><button onClick={"SUB COLUMN"} id = "subColumns" type = "button">Subtract Columns</button></td></tr>
+    
+     </table> 
+
+     <table id = "usersTable">
+    <tr id = "usersTable"><td id = "usersTable" class = "t"> </td><td id = "usersTable" class = "t"> </td></tr>
+    <tr id = "usersTable"><td id = "usersTable" class = "t"></td><td id = "usersTable" class = "t"> </td></tr>
+    </table>
+    <br></br>
+   
+    <button onClick={"FILL"} id = "fill" type = "button">Fill</button>
+    <button onClick={"CLEAR"} id = "clear" type = "button">Clear</button>
+
     </div>
   );
 }
---
+++ src/redux.js
@@ -0,0 +1,88 @@
+
+
+
+
+const redux = require("redux")
+
+function addRow()
+{
+    return{
+        type: "ADD ROW"
+    }
+}
+
+function addColumns()
+{
+    return{
+        type: "ADD COLUMN"
+    }
+}
+
+function subRow()
+{
+    return{
+        type: "SUB ROW"
+    }
+}
+
+function subColumns()
+{
+    return{
+        type: "SUB COLUMN"
+    }
+}
+
+function fill()
+{
+    return{
+        type: "FILL"
+    }
+}
+
+function clear()
+{
+    return{
+        type: "CLEAR"
+    }
+}
+
+
+function reducer(state =  {}, action)
+{
+    switch(action.type)
+    {
+        case "ADD ROW":
+        return{
+            //state + 1 row with as many columns
+        }
+        
+        case "ADD COLUMN":
+        return{
+            //state + 1 column with as many rows
+        }
+
+        case "SUB ROW":
+        return{
+            //state - 1 row along with the columns
+        }
+
+        case "SUB COLUMN":
+        return{
+            //state - 1 column along with the rows
+        }
+
+        case "FILL":
+        return{
+            //state fill the entire grid
+        }
+
+        case "CLEAR":
+        return{
+            //state clear the entire grid
+        }
+
+
+    }
+
+}
+
